77d52c7c44ae3094d8cdf2ed3c91e847b6aae705
leeken <kennyl606@gmail.com>
2012-09-15 11:09:56 -0400

    updated branch

--- WhatToWear/gen/edu/umich/whattowear/R.java
+++ WhatToWear/gen/edu/umich/whattowear/R.java
@@ -18,9 +18,19 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int jacket=0x7f020002;
+        public static final int long_sleeve=0x7f020003;
+        public static final int tshirt=0x7f020004;
+        public static final int winter_jacket=0x7f020005;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080000;
+        public static final int city_text=0x7f080002;
+        public static final int clothes_image=0x7f080005;
+        public static final int clothes_text=0x7f080004;
+        public static final int menu_settings=0x7f080006;
+        public static final int postal_input=0x7f080000;
+        public static final int submit_button=0x7f080001;
+        public static final int temp_text=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
---
+++ WhatToWear/src/edu/umich/whattowear/WeatherReport.java
@@ -0,0 +1,74 @@
+package edu.umich.whattowear;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WeatherReport {
+	private JSONObject weatherReport;
+	
+	public WeatherReport(String json) {
+		try {
+			weatherReport = new JSONObject(json);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public double getFeelLikeCelcius() {
+		double degreeCelcius = 0.0;
+		try {
+			degreeCelcius = weatherReport.getJSONObject("current_observation").getDouble("feelslike_c");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return degreeCelcius;
+	}
+	
+	public double getCelcius() {
+		double degreeCelcius = 0.0;
+		try {
+			degreeCelcius = weatherReport.getJSONObject("current_observation").getDouble("temp_c");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return degreeCelcius;
+	}
+	
+	
+	public String getCity() {
+		String city = "";
+		try {
+			city = weatherReport.getJSONObject("current_observation").getJSONObject("display_location").getString("city");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return city;
+	}
+	
+	public String getTempString() {
+		String tempString = "";
+		try {
+			tempString = weatherReport.getJSONObject("current_observation").getString("temperature_string");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return tempString;
+	}
+	
+	public String getFeelslikeString() {
+		String feelslikeString = "";
+		try {
+			feelslikeString = weatherReport.getJSONObject("current_observation").getString("feelslike_string");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return feelslikeString;
+	}
+	
+}
